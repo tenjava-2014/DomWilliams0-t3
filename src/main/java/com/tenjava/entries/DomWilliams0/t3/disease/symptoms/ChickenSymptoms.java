f5a51c6b8bcdaaaf2ba377f235f55637bdb1b7c4
Dom <dwi3774@googlemail.com>
2014-07-12 21:47:45 +0100

    Bwukbwuk

--- src/main/java/com/tenjava/entries/DomWilliams0/t3/disease/symptoms/ChickenSymptoms.java
+++ src/main/java/com/tenjava/entries/DomWilliams0/t3/disease/symptoms/ChickenSymptoms.java
@@ -4,10 +4,12 @@ import com.tenjava.entries.DomWilliams0.t3.TenJava;
 import com.tenjava.entries.DomWilliams0.t3.util.Utils;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -21,7 +23,7 @@ public class ChickenSymptoms extends BaseSymptoms
 	@Override
 	public void tick()
 	{
-		if (TenJava.RANDOM.nextFloat() < 0.3)
+		if (TenJava.RANDOM.nextFloat() < 0.5)
 		{
 			Utils.playSound(entity, Sound.FUSE);
 			entity.setVelocity(entity.getVelocity().setY(0.4));
@@ -31,19 +33,22 @@ public class ChickenSymptoms extends BaseSymptoms
 	@Override
 	public void die()
 	{
-		Firework firework = (Firework) entity.getWorld().spawnEntity(entity.getLocation(), EntityType.FIREWORK);
-		FireworkMeta meta = firework.getFireworkMeta();
-		meta.setPower(2);
-		meta.addEffect(FireworkEffect.builder().withTrail().withColor(Color.WHITE, Color.ORANGE).build());
-		firework.setFireworkMeta(meta);
-
-		entity.setVelocity(entity.getVelocity().setY(2));
+		for (int i = 0; i < 8; i++)
+		{
+			Firework firework = (Firework) entity.getWorld().spawnEntity(entity.getLocation(), EntityType.FIREWORK);
+			FireworkMeta fireworkMeta = firework.getFireworkMeta();
+			fireworkMeta.addEffect(FireworkEffect.builder().with(FireworkEffect.Type.BURST).withColor(Color.fromRGB(TenJava.RANDOM.nextInt(256), TenJava.RANDOM.nextInt(256), TenJava.RANDOM.nextInt(256))).build());
+			fireworkMeta.setPower(0);
+			firework.setFireworkMeta(fireworkMeta);
+		}
+		entity.setVelocity(entity.getVelocity().setY(1.3));
 
 		new BukkitRunnable()
 		{
 			@Override
 			public void run()
 			{
+				entity.getWorld().dropItemNaturally(entity.getLocation(), new ItemStack(Material.EGG));
 				entity.remove();
 			}
 		}.runTaskLater(TenJava.INSTANCE, 20L);
